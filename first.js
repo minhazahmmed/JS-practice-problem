d18a01ac3c83ac422e8289336dc05879dcb21db6
minhazahmmed <minhazahmmed231@gmail.com>
2025-08-22 10:09:47 +0600

    button action added

---
+++ first.js
@@ -0,0 +1,18 @@
+
+
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+function makeViolet(){
+    document.body.style.backgroundColor = 'violet';
+}
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
